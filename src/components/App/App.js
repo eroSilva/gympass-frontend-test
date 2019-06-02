1619048012dc0bf0fcdf02bd5d17e31ad0e89f70
eroSilva <eronildo.silva@outlook.com>
2019-06-02 20:54:10 -0300

    Created basic structure

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,34 @@
-import React from 'react';
-import './App.scss';
+import React, { useState, useEffect } from 'react';
+import style from './App.module.scss';
+import Repositories from '../Repositories';
+import Env from '../../Env.js';
 
 function App() {
+  const [userData, setUserData] = useState([]);
+
+  useEffect(() => {
+    async function loadUserData() {
+      const responseUserData = await fetch(`https://api.github.com/users/${Env.userLogin}`);
+      const responseUserDataJSON = await responseUserData.json();
+
+      setUserData(responseUserDataJSON)
+    }
+    
+    loadUserData()
+  }, [])
+
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <main className={style.Main}>
+      <header className={style.Header}>
+        <figure className={style.Photo}>
+          <img src={userData.avatar_url} alt={`${userData.name} | ${userData.bio}`}/>
+        </figure>
+        <h2 className={style.UserName}>{userData.name}</h2>
+        <h1 className={style.Title}>Github User Infos</h1>
+        <p className={style.Description}>Detalhes públicos do meu perfil do Github.</p>
+      </header>
+      <Repositories />
+    </main>
   );
 }
 
