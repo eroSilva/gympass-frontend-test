304721b483c6e4432665b0c327b06f3feb1ee08a
eroSilva <eronildo.silva@outlook.com>
2019-06-02 20:57:17 -0300

    Created Repository Component to show specific details of a repository

---
+++ src/components/Repository/Repository.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import style from './Repository.module.scss';
+import Commits from '../Commits';
+
+function Repository(state) {
+  return (
+    <div className={`${style.Modal} ${state.modalOpen ? style.isOpen : ''}`}>
+      <button className={style.CloseButton} onClick={() => state.cleanRepositoryDetails()}>X</button>
+      {state.repositoryData.length !== 0 ? <Commits repoName={state.repositoryData.name} repoDescription={state.repositoryData.description}/> : null}
+    </div>
+  );
+}
+
+export default Repository;
